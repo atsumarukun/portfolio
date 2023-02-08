4f1d227eede76d866451b824d167e0738cb4e2c2
atsumarukun <atys.smz@gmail.com>
2023-02-09 02:12:31 +0900

    Create top animation.

---
+++ scripts/start_animation.js
@@ -0,0 +1,15 @@
+function topAnimation() {
+    const title = 'ATSUMARUKUN'
+    for (const i in title) {
+        $('.js_ttl').append(`<span>${title[i]}</span>`);
+    }
+
+    const timeline = gsap.timeline();
+    timeline.fromTo('.js_ttl span', 0.5, {x: 50, y: 50, rotation: 90}, {x: 0, y: 0, rotation: 0, stagger: 0.1, ease: Power1.easeOut})
+    .to('.js_topAnimation_bg', 1.25, {x: '100%', ease: Power4.easeOut}, 1.75)
+    .to('.js_ttl span', 1, {color: '#000000', stagger: 0.01, ease: Power4.easeOut}, 1.75)
+    .to('.js_topAnimation', 1.25, {opacity: 0, ease: Power4.easeOut}, 3)
+    .to('.js_topAnimation', 0.01, {x: '100%'}, 4.25);
+}
+
+topAnimation();
